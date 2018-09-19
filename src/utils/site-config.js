60df08d855defe78a08dbdfb5770546c7c80a666
Justin Hall <justin.r.hall@gmail.com>
2018-09-19 14:45:27 -0600

    fixup! Remove manifest and offline Gatsby plugins

--- src/utils/site-config.js
+++ src/utils/site-config.js
@@ -1,9 +1,7 @@
 const title = 'Password Lense';
 const description = 'Reveal character types in a password';
-const pwaShortName = 'PWL';
 
 module.exports = {
   title,
   description,
-  pwaShortName,
 };
