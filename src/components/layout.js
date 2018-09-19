dd7c67654d5b8de363db70bc588df5c86c651d00
Justin Hall <justin.r.hall@gmail.com>
2018-09-19 13:35:08 -0600

    Remove unnecessary flex display from Layout

--- src/components/layout.js
+++ src/components/layout.js
@@ -93,8 +93,6 @@ const Layout = ({ children }) => (
         </Helmet>
         <div
           className={css`
-            display: flex;
-            flex-direction: column;
             min-height: 100vh;
             padding: 0 1rem;
           `}
