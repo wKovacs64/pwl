137f59855f73b7ef2c743be2cc117af42b4c9207
Justin Hall <justin.r.hall@gmail.com>
2018-09-26 09:47:54 -0600

    Refactor auto update mechanism to be more reusable

--- src/components/auto-updater.js
+++ src/components/auto-updater.js
@@ -1,24 +1,32 @@
-import React, { Component } from 'react';
+import { Component } from 'react';
 import PropTypes from 'prop-types';
-import { css } from 'react-emotion';
-import axios from 'axios';
-import mq from '../utils/mq';
 
 class AutoUpdater extends Component {
   static propTypes = {
-    siteTitle: PropTypes.string.isRequired,
-    indexUrl: PropTypes.string.isRequired,
-    isNewer: PropTypes.func.isRequired,
-    pollingIntervalMs: PropTypes.number.isRequired,
+    children: PropTypes.func,
+    // hasUpdate is a function provided by the consumer to determine whether or
+    // not there is an updated version of the code available. This typically
+    // involves fetching a non-cached version of some data that can be compared
+    // to the current/running version. It should return true if the data is
+    // different or false if it's unchanged.
+    hasUpdate: PropTypes.func.isRequired,
+    pollingIntervalMs: PropTypes.number,
   };
 
-  state = {
-    newCodeAvailable: false,
+  static defaultProps = {
+    children: () => null,
+    pollingIntervalMs: 3600000, // 1 hour
   };
+
+  static initialState = {
+    error: '',
+    updateAvailable: false,
+  };
+
+  state = AutoUpdater.initialState;
 
   componentDidMount() {
     if (typeof window !== 'undefined') {
-      this.parser = new DOMParser();
       this.checkForUpdates();
       this.interval = window.setInterval(
         this.checkForUpdates,
@@ -38,86 +46,24 @@ class AutoUpdater extends Component {
   };
 
   checkForUpdates = async () => {
-    const { isNewer } = this.props;
-    const { newCodeAvailable } = this.state;
+    const { hasUpdate } = this.props;
+    const { updateAvailable } = this.state;
 
-    if (!newCodeAvailable) {
-      try {
-        const res = await axios.get(this.props.indexUrl, {
-          headers: { Pragma: 'no-cache' },
-        });
-
-        if (res.data) {
-          const remoteDocument = this.parser.parseFromString(
-            res.data,
-            'text/html',
-          );
-
-          if (isNewer(remoteDocument)) {
-            this.setState({ newCodeAvailable: true });
-            this.clearUpdateCheckInterval();
-          }
-        }
-      } catch (err) {
-        // no-op: we don't really care if the update checks fail
+    try {
+      if (!updateAvailable && (await hasUpdate())) {
+        this.setState({ ...AutoUpdater.initialState, updateAvailable: true });
+        this.clearUpdateCheckInterval();
       }
-    }
-  };
-
-  reloadApp = e => {
-    e.preventDefault();
-    if (typeof window !== 'undefined') {
-      window.location.reload(true);
+    } catch (err) {
+      this.setState({ ...AutoUpdater.initialState, error: err.message });
     }
   };
 
   render() {
-    const { siteTitle } = this.props;
+    const { children } = this.props;
+    const { error, updateAvailable } = this.state;
 
-    if (this.state.newCodeAvailable) {
-      return (
-        <div
-          aria-live="polite"
-          role="alert"
-          className={css`
-            position: fixed;
-            top: 0;
-            left: 0;
-            width: 100vw;
-            z-index: 9999;
-            box-shadow: 4px 4px 8px 0px rgba(0, 0, 0, 0.2);
-            ${mq.md(css`
-              top: 1rem;
-              left: unset;
-              right: 1rem;
-              width: auto;
-            `)};
-          `}
-        >
-          <button
-            type="button"
-            onClick={this.reloadApp}
-            className={css`
-              color: #b3efff;
-              background-color: #1c304a;
-              border: none;
-              padding: 1rem;
-              width: 100%;
-              transition: color 0.3s ease, background-color 0.3s ease;
-              & :hover {
-                cursor: pointer;
-                color: #1c304a;
-                background-color: #b3efff;
-              }
-            `}
-          >
-            A new version of {siteTitle} is available!
-          </button>
-        </div>
-      );
-    }
-
-    return null;
+    return children({ error, updateAvailable });
   }
 }
 
