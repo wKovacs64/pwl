d354eccf9f469e890397609f8c93731c3bca0737
Justin Hall <justin.r.hall@gmail.com>
2018-12-09 10:56:05 -0700

    Add noscript SSR hack to appease Lighthouse

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -4,4 +4,14 @@
  * See: https://www.gatsbyjs.org/docs/ssr-apis/
  */
 
-// You can delete this file if you're not using it
+import React from 'react';
+
+// Hack: appease the Lighthouse PWA audit, even though a real noscript tag
+// exists in the Layout component.
+export const onRenderBody = ({ setPreBodyComponents }) => {
+  setPreBodyComponents([
+    <noscript key="lighthouse-noscript" hidden>
+      Please enable JavaScript.
+    </noscript>,
+  ]);
+};
