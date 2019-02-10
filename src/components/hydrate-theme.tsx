f882bb47245091e6565558ecb9a7fc194b608851
Justin Hall <justin.r.hall@gmail.com>
2019-02-10 11:45:18 -0700

    Move theme hydration to Gatsby SSR setPreBodyComponents API

--- src/components/hydrate-theme.tsx
+++ src/components/hydrate-theme.tsx
@@ -1,9 +1,7 @@
-/* eslint-disable */
 import React from 'react';
-import PropTypes from 'prop-types';
 
 // https://raw.githubusercontent.com/donavon/use-dark-mode/develop/noflash.js.txt
-const hydrateTheme = `
+const hydrateThemeScript = `
   (function() {
     // Change these if you use something different in your hook.
     var storageKey = 'darkMode';
@@ -43,40 +41,9 @@ const hydrateTheme = `
   })();
 `;
 
-export default function HTML(props) {
-  return (
-    <html {...props.htmlAttributes}>
-      <head>
-        <meta charSet="utf-8" />
-        <meta httpEquiv="x-ua-compatible" content="ie=edge" />
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1, shrink-to-fit=no"
-        />
-        {props.headComponents}
-      </head>
-      <body {...props.bodyAttributes}>
-        <script dangerouslySetInnerHTML={{ __html: hydrateTheme }} />
-        {props.preBodyComponents}
-        <noscript key="noscript" id="gatsby-noscript">
-          This app works best with JavaScript enabled.
-        </noscript>
-        <div
-          key={`body`}
-          id="___gatsby"
-          dangerouslySetInnerHTML={{ __html: props.body }}
-        />
-        {props.postBodyComponents}
-      </body>
-    </html>
-  );
-}
+const HydrateTheme: React.FunctionComponent = () => (
+  // eslint-disable-next-line react/no-danger
+  <script dangerouslySetInnerHTML={{ __html: hydrateThemeScript }} />
+);
 
-HTML.propTypes = {
-  htmlAttributes: PropTypes.object,
-  headComponents: PropTypes.array,
-  bodyAttributes: PropTypes.object,
-  preBodyComponents: PropTypes.array,
-  body: PropTypes.string,
-  postBodyComponents: PropTypes.array,
-};
+export default HydrateTheme;
