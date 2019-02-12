a549d080dafed75537eb714d7b026ae861ddc5e9
Justin Hall <justin.r.hall@gmail.com>
2019-02-12 12:54:23 -0700

    Remove legacy @emotion/styled wrapper

--- src/utils/styled.ts
+++
@@ -1,6 +0,0 @@
-import styled, { CreateStyled } from '@emotion/styled';
-import { Theme } from '../theme';
-
-// https://emotion.sh/docs/typescript#define-a-theme
-
-export default styled as CreateStyled<Theme>;
