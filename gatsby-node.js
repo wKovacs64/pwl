e017cdcdac370be531c9177326047ca972bb306d
Justin Hall <justin.r.hall@gmail.com>
2020-02-22 13:08:13 -0700

    fix(hot-loader): alias react-dom to @hot-loader/react-dom in development

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,4 +4,14 @@
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
 
-// You can delete this file if you're not using it
+exports.onCreateWebpackConfig = ({ stage, actions }) => {
+  if (stage.startsWith('develop')) {
+    actions.setWebpackConfig({
+      resolve: {
+        alias: {
+          'react-dom': '@hot-loader/react-dom',
+        },
+      },
+    });
+  }
+};
