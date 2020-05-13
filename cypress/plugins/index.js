e42182cc1af8b807d06c0e48abeff0dc9a02da5a
Justin Hall <justin.r.hall@gmail.com>
2020-05-13 16:03:32 -0600

    test(e2e): refactor Cypress+TypeScript setup (#142)
    * test(e2e): remove @cypress/browserify-preprocessor and tsify
    
    * chore(ts): create separate tsconfig.json for cypress

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,8 +1,17 @@
-const browserify = require('@cypress/browserify-preprocessor');
+// ***********************************************************
+// This example plugins/index.js can be used to load plugins
+//
+// You can change the location of this file or turn off loading
+// the plugins file with the 'pluginsFile' configuration option.
+//
+// You can read more here:
+// https://on.cypress.io/plugins-guide
+// ***********************************************************
 
-module.exports = (on /* , config */) => {
-  const options = browserify.defaultOptions;
-  options.browserifyOptions.plugin.push('tsify');
+// This function is called when a project is opened or re-opened (e.g. due to
+// the project's config changing)
 
-  on('file:preprocessor', browserify(options));
+module.exports = (/* on, config */) => {
+  // `on` is used to hook into various events Cypress emits
+  // `config` is the resolved Cypress config
 };
