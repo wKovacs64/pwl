c0789ae3b95caa23616e64b1704bc0297c6ffcee
Justin Hall <justin.r.hall@gmail.com>
2020-05-24 17:19:35 -0600

    refactor(dev): replace React hot reload with Fast Refresh

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,14 +4,4 @@
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
 
-exports.onCreateWebpackConfig = ({ stage, actions }) => {
-  if (stage.startsWith('develop')) {
-    actions.setWebpackConfig({
-      resolve: {
-        alias: {
-          'react-dom': '@hot-loader/react-dom',
-        },
-      },
-    });
-  }
-};
+// You can delete this file if you're not using it
