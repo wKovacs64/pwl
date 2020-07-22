9d6eb7f82e8ebcfd116e4d0e424a8afc130ef645
Justin Hall <justin.r.hall@gmail.com>
2020-07-22 10:05:02 -0600

    refactor: move CSS and font imports to gatsby-browser

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -3,5 +3,6 @@
  *
  * See: https://www.gatsbyjs.org/docs/browser-apis/
  */
-
-// You can delete this file if you're not using it
+import 'typeface-nunito';
+import 'typeface-source-sans-pro';
+import '@wkovacs64/normalize.css';
