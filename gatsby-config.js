224ea056867f922396e05103874662444bbc3f79
Justin Hall <justin.r.hall@gmail.com>
2020-12-02 13:36:01 -0700

    build(dev): enable experimental Gatsby feature flags for an improved dev experience (#193)

--- gatsby-config.js
+++ gatsby-config.js
@@ -43,4 +43,10 @@ module.exports = {
     },
     'gatsby-plugin-offline',
   ],
+  flags: {
+    FAST_DEV: true,
+    FAST_REFRESH: true,
+    PRESERVE_WEBPACK_CACHE: true,
+    PRESERVE_FILE_DOWNLOAD_CACHE: true,
+  },
 };
