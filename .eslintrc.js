6132615c5c53899209304c526eb74b8be80d1ab8
Justin Hall <justin.r.hall@gmail.com>
2020-12-01 18:32:34 -0700

    chore(deps): update emotion to v11

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,12 +7,4 @@ module.exports = {
     'prettier/react',
     'prettier/@typescript-eslint',
   ],
-  plugins: ['emotion'],
-  rules: {
-    // 'emotion/jsx-import': 'error',
-    'emotion/no-vanilla': 'error',
-    'emotion/import-from-emotion': 'error',
-    'emotion/styled-import': 'error',
-    'emotion/syntax-preference': ['error', 'string'],
-  },
 };
--- src/components/results.tsx
+++ src/components/results.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { css } from '@emotion/core';
+import { css } from '@emotion/react';
 import styled from '@emotion/styled';
 import { ColorMap, LabelMap } from '../legend';
 import PasswordThroughLense from './password-through-lense';
