9637b449e97f77c00dc59b22e041b864d43785cf
Justin Hall <justin.r.hall@gmail.com>
2020-12-24 11:31:25 -0700

    chore(deps): update fontsource fonts to v4

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -3,6 +3,6 @@
  *
  * See: https://www.gatsbyjs.org/docs/browser-apis/
  */
-import 'fontsource-nunito/latin.css';
-import 'fontsource-source-sans-pro/latin.css';
+import '@fontsource/nunito/latin.css';
+import '@fontsource/source-sans-pro/latin.css';
 import '@wkovacs64/normalize.css';
