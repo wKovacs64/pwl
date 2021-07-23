10755fc31bdf5b00f2628177cd29b84b79df52c5
Justin Hall <justin.r.hall@gmail.com>
2021-07-23 10:03:24 -0600

    refactor: leverage XState models more (#251)

--- src/machines/pwned-info.machine.ts
+++ src/machines/pwned-info.machine.ts
@@ -1,42 +1,59 @@
-import { createMachine, DoneInvokeEvent } from 'xstate';
 import { createModel } from 'xstate/lib/model';
 import { pwnedPassword } from 'hibp';
 
-const pwnedInfoModel = createModel<PwnedInfoContext, PwnedInfoResultsEvent>({
+const initialContext = {
   numPwns: -1,
   error: false,
-});
+};
 
-const assignNumPwns = pwnedInfoModel.assign({
-  numPwns: (context, event) =>
-    typeof event.data === 'number' ? event.data : context.numPwns,
-});
+const creators = {
+  events: {
+    getPwnedInfo: (password: string) => ({ password }),
+    setNumPwns: (numPwns: number) => ({ numPwns }),
+    setError: () => ({}),
+  },
+};
+
+export const pwnedInfoModel = createModel(initialContext, creators);
+
+const assignNumPwns = pwnedInfoModel.assign(
+  { numPwns: (_, event) => event.numPwns },
+  'setNumPwns',
+);
+
 const assignError = pwnedInfoModel.assign({ error: true });
+
 const reset = pwnedInfoModel.reset();
 
-export const pwnedInfoMachine = createMachine<
-  PwnedInfoContext,
-  PwnedInfoRequestEvent,
-  PwnedInfoState
->({
+export const pwnedInfoMachine = pwnedInfoModel.createMachine({
   id: 'pwnedInfo',
   initial: 'idle',
   context: pwnedInfoModel.initialContext,
-  on: { REQUEST: 'loading' },
+  on: { getPwnedInfo: 'loading' },
   states: {
     idle: {},
     loading: {
       entry: reset,
       invoke: {
         id: 'pwnedPassword',
-        src: (_, event) => pwnedPassword(event.payload),
-        onDone: {
-          target: 'success',
-          actions: [reset, assignNumPwns],
+        src: (_, event) => async (send) => {
+          if (event.type !== 'getPwnedInfo') return;
+          try {
+            const numPwns = await pwnedPassword(event.password);
+            send(pwnedInfoModel.events.setNumPwns(numPwns));
+          } catch (err: unknown) {
+            send(pwnedInfoModel.events.setError());
+          }
         },
-        onError: {
-          target: 'failure',
+      },
+      on: {
+        setNumPwns: {
+          actions: [reset, assignNumPwns],
+          target: 'success',
+        },
+        setError: {
           actions: [reset, assignError],
+          target: 'failure',
         },
       },
     },
@@ -44,27 +61,3 @@ export const pwnedInfoMachine = createMachine<
     failure: {},
   },
 });
-
-interface PwnedInfoContext {
-  numPwns: number;
-  error: boolean;
-}
-
-type PwnedInfoState =
-  | { value: 'idle'; context: PwnedInfoContext }
-  | { value: 'loading'; context: PwnedInfoContext }
-  | { value: 'success'; context: PwnedInfoContext }
-  | { value: 'failure'; context: PwnedInfoContext };
-
-// external/incoming events
-interface PwnedInfoRequestEvent {
-  type: 'REQUEST';
-  payload: string;
-}
-
-// internal events (results from invoking the pwnedPassword promise)
-type PwnedInfoPwnedPasswordSuccessEvent = DoneInvokeEvent<number>;
-type PwnedInfoPwnedPasswordFailureEvent = DoneInvokeEvent<Error>;
-type PwnedInfoResultsEvent =
-  | PwnedInfoPwnedPasswordSuccessEvent
-  | PwnedInfoPwnedPasswordFailureEvent;
