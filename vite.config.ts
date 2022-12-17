0c64edc9c0d43f0870f08c0fea7420593084ab41
Justin Hall <justin.r.hall@gmail.com>
2022-12-17 14:28:23 -0700

    chore(deps): update Vite to v4

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import react from '@vitejs/plugin-react-swc';
 import { VitePWA } from 'vite-plugin-pwa';
 import gitCommit from 'git-current-commit';
 
