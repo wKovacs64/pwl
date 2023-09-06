e56d3f4a4a4a430810564546a457a98a390c50ce
Justin Hall <justin.r.hall@gmail.com>
2023-09-06 14:37:48 -0600

    chore: restore PWA functionality
    This was removed by mistake in #362.

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './app';
 import AppProviders from './app-providers';
+import './pwa';
 
 const container = document.getElementById('root');
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
